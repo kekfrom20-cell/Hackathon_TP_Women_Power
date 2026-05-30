920ef3b30bf9ab1fb986ce890fcb56f990ca2514
Ksenia <kseniabb006@gmail.com>
2026-05-30 13:50:05 +0300

    Mail class

--- src/mail.py
+++ src/mail.py
@@ -1,12 +1,15 @@
-from dataclasses import dataclass 
- 
- 
-@dataclass 
-class Mail: 
-    filename: str 
-    subject: str 
-    sender: str 
-    body: str 
- 
-    def is_empty(self) -> bool: 
-        return len(self.body.strip()) == 0
+from dataclasses import dataclass
+
+
+@dataclass
+class Mail:
+    filename: str
+    subject: str
+    sender: str
+    body: str
+
+    def is_empty(self) -> bool:
+        return len((self.subject + self.body).strip()) == 0
+
+    def full_text(self) -> str:
+        return f"{self.subject}\n{self.body}"
